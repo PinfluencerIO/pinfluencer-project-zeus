e4d3c7e7a31a4580a1800721662248a816da1c9b
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-12 16:45:39 +0100

    removed semi colon within proxy lambda call

--- functions/brands/get/get_brands.py
+++ functions/brands/get/get_brands.py
@@ -4,4 +4,4 @@ except:
     from layers.python.web.lambda_layer.legacy_get_brands import get_brands
 
 def lambda_handler(event, context):
-    return get_brands(event, context);
+    return get_brands(event, context)
