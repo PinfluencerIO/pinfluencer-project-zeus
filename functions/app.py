1959bfade4093599e7b6e5c52c927bd54e6cd0c9
Dom Farr <dominicfarr@gmail.com>
2021-10-16 10:49:14 -0300

    fixed import of util module

--- functions/app.py
+++ functions/app.py
@@ -1,10 +1,8 @@
-try:
-    from utils import util_web, util_log
-except Exception as e:
-    print(e)
+import utils.util_web as web
+import utils.util_log as log
 
 
 def lambda_handler(event, context):
-    response = util_web.Controller.process(event)
-    util_log.logger.info("response: %s" % response)
+    response = web.Controller.process(event)
+    log.logger.info("response: %s" % response)
     return response
--- functions/utils/util_web.py
+++ functions/utils/util_web.py
@@ -1,6 +1,6 @@
-from .util_log import print_exception
-from .util_http import HttpUtils
-from .util_db import *
+import utils.util_log as log
+import utils.util_http as http
+import utils.util_db as db
 
 class Controller():
     def __init__(self):
@@ -13,20 +13,20 @@ class Controller():
             resource = event['rawPath'][1:-1]
             # Handle CRUD 
             if http_method == 'post':
-                return HttpUtils.respond(res=f"Create {resource}")
+                return http.HttpUtils.respond(res=f"Create {resource}")
             elif http_method == 'get':
-                formatted = formatRecords(executeQuery(f'SELECT * FROM {resource}')['records'])
-                return HttpUtils.respond(res=f"Read {resource} Results:\n {formatted}")
+                formatted = db.formatRecords(db.executeQuery(f'SELECT * FROM {resource}')['records'])
+                return http.HttpUtils.respond(res=f"Read {resource} Results:\n {formatted}")
             elif http_method == 'put':
-                return HttpUtils.respond(res=f"Update {resource}")
+                return http.HttpUtils.respond(res=f"Update {resource}")
             elif http_method == 'delete':
-                return HttpUtils.respond(res=f"Delete {resource}")
+                return http.HttpUtils.respond(res=f"Delete {resource}")
 
-            return HttpUtils.respond(err="Unsupported action", err_code=400, res=f"Unsupported action {http_method}")
+            return http.HttpUtils.respond(err="Unsupported action", err_code=400, res=f"Unsupported action {http_method}")
 
         except Exception as e:
-            print_exception(e)
-            return HttpUtils.respond(err=e, err_code=404)
+            log.print_exception(e)
+            return http.HttpUtils.respond(err=e, err_code=404)
 
     @staticmethod
     def extract_http_method(event):
