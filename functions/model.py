4a47c87d1ac50c572560d687103a70d7d1cc3a6b
Dom Farr <dominicfarr@gmail.com>
2021-10-19 10:14:42 -0300

    changed model to use  dataclass with helper function to convert to json

--- functions/model.py
+++ functions/model.py
@@ -1,25 +1,39 @@
 import datetime
+import json
 import uuid
+
+from attr import dataclass
 
 
 class Brand:
+    id_: uuid
+    created: datetime
+    name: str
+    bio: str
+    website: str
+    email: str
+    version: int
 
-    def __init__(self, id_: uuid, created: datetime, name: str, bio: str, website:str, email:str, version: int = 0):
-        self.id_ = id_
-        self.created = created
-        self.name = name
-        self.bio = bio
-        self.website = website
-        self.email = email
-        self.version = version
+    def as_json(self):
+        return json.dumps(self.__dict__, default=str)
 
 
+@dataclass()
 class Product:
+    id_: uuid
+    created: datetime
+    name: str
+    description: str
+    image: str
+    version: int
 
-    def __init__(self, id_: uuid, created: datetime, name: str, description: str, image: str, version: int = 0):
-        self.id_ = id_
-        self.created = created
-        self.name = name
-        self.description = description
-        self.image = image
-        self.version = version
+    def as_json(self):
+        return json.dumps(self.__dict__, default=str)
+
+# example Product
+# p = Product(id_=uuid.uuid4(),
+#             created=datetime.datetime.now(),
+#             name='Product Name',
+#             description='product description',
+#             image='key to image in s3',
+#             version=1)
