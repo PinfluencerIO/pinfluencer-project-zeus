f756f97e94e337b9cfbaebecb6b8e09df92a50ce
Dom Farr <dominicfarr@gmail.com>
2021-10-19 05:43:56 -0300

    add feed endpoint into template. hooked up to lamdbd. lambda returns empty values at this point

--- functions/app.py
+++ functions/app.py
@@ -1,7 +1,3 @@
-from functions import util_web
-
 
 def lambda_handler(event, context):
-    response = util_web.Controller.process(event)
-    print("response: %s" % response)
-    return response
+    return ""
