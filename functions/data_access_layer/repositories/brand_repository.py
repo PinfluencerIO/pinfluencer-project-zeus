87d2591578ed17a16ebb42ce6324369b8c7182bd
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-26 01:23:48 +0100

    altered signature of brand repository methods

--- functions/data_access_layer/repositories/brand_repository.py
+++ functions/data_access_layer/repositories/brand_repository.py
@@ -1,5 +1,5 @@
 from functions.data_access_layer.data_manager import DataManager
-from functions.data_access_layer.models.product_model import ProductModel
+from functions.data_access_layer.models.brand_model import BrandModel
 
 
 class BrandRepository:
@@ -9,14 +9,14 @@ class BrandRepository:
     def __init__(self, session_manager: DataManager):
         self.__session_maker = session_manager
 
-    def get(self, id: str) -> ProductModel:
-        return self.__session_maker.session.query(ProductModel).filter(ProductModel.id == id)
+    def get(self, id: str) -> BrandModel:
+        return self.__session_maker.session.query(BrandModel).filter(BrandModel.id == id)
 
-    def get_all(self) -> list[ProductModel]:
-        return self.__session_maker.session.query(ProductModel)
+    def get_all(self) -> list[BrandModel]:
+        return self.__session_maker.session.query(BrandModel)
 
-    def create(self, data: ProductModel) -> ProductModel:
+    def create(self, data: BrandModel) -> BrandModel:
         return self.__session_maker.session.add(data)
 
-    def update(self, id: str, data: ProductModel) -> bool:
+    def update(self, id: str, data: BrandModel) -> bool:
         raise NotImplemented
