8804274485592742403960cd2f8089ae7b8228ab
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-11-03 19:18:32 +0000

    removed default value from created column

--- src/data_access_layer/entities/base_entity.py
+++ src/data_access_layer/entities/base_entity.py
@@ -13,4 +13,4 @@ class BaseEntity:
     __tablename__: str
 
     id: str = Column(type_=String(length=36), primary_key=True, default=uuid.uuid4, nullable=False)
-    created: datetime = Column(DateTime, default=datetime.now, nullable=False)
+    created: datetime = Column(DateTime, nullable=False)
