ff41d8522069e36a7da340b349f1333e17372752
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-11-05 00:17:27 +0000

    added json formatting

---
+++ src/domain/models/model_extensions.py
@@ -0,0 +1,20 @@
+import dataclasses
+import json
+
+import src.domain.models.base_model as base
+
+
+class ModelExtensions:
+    @staticmethod
+    def list_to_json(models) -> str:
+        """
+        @type models: list[base.BaseModel]
+        """
+        return json.dumps(list(map(ModelExtensions.as_dict, models)), indent=4)
+
+    @staticmethod
+    def as_dict(model) -> dict:
+        """
+        @type model: base.BaseModel
+        """
+        return dataclasses.asdict(model)
