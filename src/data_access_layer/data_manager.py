14d4ed0d821abc04c2dd28c22067f44d2fc92600
Dom Farr <dominicfarr@gmail.com>
2021-12-03 14:08:44 -0400

    changed db connect string

--- src/data_access_layer/data_manager.py
+++ src/data_access_layer/data_manager.py
@@ -14,13 +14,16 @@ class DataManager(DataManagerInterface):
 
     def __init__(self, status_manager: RequestStatusManager):
         print("new data manager constructed")
+        # engine = create_engine(
+        #     'postgresql+auroradataapi://:@/pinfluencerdb',
+        #     connect_args={
+        #         'aurora_cluster_arn': os.environ['DB_CLUSTER_ARN'],
+        #         'secret_arn': os.environ['DB_SECRET_ARN']
+        #     }
+        # )
+        THE_PASSWORD = os.environ['db_password']
         engine = create_engine(
-            'postgresql+auroradataapi://:@/pinfluencerdb',
-            connect_args={
-                'aurora_cluster_arn': os.environ['DB_CLUSTER_ARN'],
-                'secret_arn': os.environ['DB_SECRET_ARN']
-            }
-        )
+            "postgresql://postgres:%s@pinfluencer.czqff0jhbhz3.eu-west-2.rds.amazonaws.com/pinfluencer" % THE_PASSWORD)
         self.__engine = engine
         session = sessionmaker(bind=self.__engine, autocommit=False)
         self.__session = session()
