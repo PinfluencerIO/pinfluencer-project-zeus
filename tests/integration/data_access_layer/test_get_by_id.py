04a835778c154d460344bcfd6f41e71a6f95bde4
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-17 21:56:28 +0000

    add additional tests for get by id tests

--- tests/integration/data_access_layer/test_get_by_id.py
+++ tests/integration/data_access_layer/test_get_by_id.py
@@ -15,5 +15,13 @@ def test_load_brand_by_id_item_when_brand_can_be_found():
 
 def test_load_brand_item_by_id_when_brand_cannot_be_found():
     data_manager = InMemorySqliteDataManager()
+    brand = brand_generator(1)
+    data_manager.create_fake_data([brand])
+    item = load_by_id(resource=types['brand']['type'], data_manager=data_manager, id_=str(uuid.uuid4()))
+    assert item is None
+
+
+def test_load_brand_item_by_id_when_no_brands_exist():
+    data_manager = InMemorySqliteDataManager()
     item = load_by_id(resource=types['brand']['type'], data_manager=data_manager, id_=str(uuid.uuid4()))
     assert item is None
