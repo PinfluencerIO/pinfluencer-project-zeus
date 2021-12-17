0c4dadd901824dec0ddf7cb32c1fc9869e35c0de
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-17 22:49:10 +0000

    made tests more dry

--- tests/integration/data_access_layer/test_get_brand_for_auth_user.py
+++ tests/integration/data_access_layer/test_get_brand_for_auth_user.py
@@ -3,17 +3,13 @@ from tests.unit import InMemorySqliteDataManager, brand_generator
 
 
 def test_load_brand_by_auth_id_item_when_brand_can_be_found():
-    data_manager = InMemorySqliteDataManager()
-    brand = brand_generator(1)
-    data_manager.create_fake_data([brand])
+    [data_manager, brand] = setup_database()
     item = load_brand_for_authenticated_user(data_manager=data_manager, auth_user_id=brand.auth_user_id)
     assert brand.as_dict() == item.as_dict()
 
 
 def test_load_brand_item_by_auth_id_when_brand_cannot_be_found():
-    data_manager = InMemorySqliteDataManager()
-    brand = brand_generator(1)
-    data_manager.create_fake_data([brand])
+    [data_manager, _] = setup_database
     item = load_brand_for_authenticated_user(data_manager=data_manager, auth_user_id="invalid id")
     assert item is None
 
@@ -22,3 +18,10 @@ def test_load_brand_item_by_auth_id_when_no_brands_exist():
     data_manager = InMemorySqliteDataManager()
     item = load_brand_for_authenticated_user(data_manager=data_manager, auth_user_id="invalid id")
     assert item is None
+
+
+def setup_database():
+    data_manager = InMemorySqliteDataManager()
+    brand = brand_generator(1)
+    data_manager.create_fake_data([brand])
+    return [data_manager, brand]
--- tests/integration/data_access_layer/test_get_by_id.py
+++ tests/integration/data_access_layer/test_get_by_id.py
@@ -6,17 +6,13 @@ from tests.unit import InMemorySqliteDataManager, brand_generator
 
 
 def test_load_brand_by_id_item_when_brand_can_be_found():
-    data_manager = InMemorySqliteDataManager()
-    brand = brand_generator(1)
-    data_manager.create_fake_data([brand])
+    [data_manager, brand] = setup_database()
     item = load_by_id(resource=types['brand']['type'], data_manager=data_manager, id_=brand.id)
     assert brand.as_dict() == item.as_dict()
 
 
 def test_load_brand_item_by_id_when_brand_cannot_be_found():
-    data_manager = InMemorySqliteDataManager()
-    brand = brand_generator(1)
-    data_manager.create_fake_data([brand])
+    [data_manager, _] = setup_database()
     item = load_by_id(resource=types['brand']['type'], data_manager=data_manager, id_=str(uuid.uuid4()))
     assert item is None
 
@@ -25,3 +21,10 @@ def test_load_brand_item_by_id_when_no_brands_exist():
     data_manager = InMemorySqliteDataManager()
     item = load_by_id(resource=types['brand']['type'], data_manager=data_manager, id_=str(uuid.uuid4()))
     assert item is None
+
+
+def setup_database():
+    data_manager = InMemorySqliteDataManager()
+    brand = brand_generator(1)
+    data_manager.create_fake_data([brand])
+    return [data_manager, brand]
