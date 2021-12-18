99d651150d88617e491c1af8944fec9d60525aa5
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-18 01:16:13 +0000

    added tests for get all products for brand

---
+++ tests/integration/data_access_layer/test_get_all_products_for_brand.py
@@ -0,0 +1,36 @@
+import uuid
+
+from src.data_access_layer import to_list
+from src.data_access_layer.read_data_access import load_all_products_for_brand_id
+from tests.unit import InMemorySqliteDataManager, brand_generator, product_generator
+
+
+def test_load_products_for_brand_when_many_products_are_available_for_many_brands():
+    brands = [brand_generator(1)]
+    products = [product_generator(1, brands[0]),
+                product_generator(2, brands[0])]
+    data_manager = setup_database(brands, products)
+    assert_test(data_manager, products, brands[0].id)
+
+
+def test_load_products_for_brand_when_no_products_are_available():
+    brands = [brand_generator(1)]
+    data_manager = setup_database(brands, [])
+    assert_test(data_manager, [], brands[0].id)
+
+
+def test_load_products_for_brand_when_brands_are_available():
+    data_manager = setup_database([], [])
+    assert_test(data_manager, [], str(uuid.uuid4()))
+
+
+def setup_database(brands, products):
+    data_manager = InMemorySqliteDataManager()
+    data_manager.create_fake_data(brands)
+    data_manager.create_fake_data(products)
+    return data_manager
+
+
+def assert_test(data_manager, expected, id_):
+    items = load_all_products_for_brand_id(data_manager=data_manager, id_=id_)
+    assert to_list(expected) == to_list(items)
