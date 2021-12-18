b67108d98dea9e2c11538d64b13a9dcc49b8f36b
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-18 00:42:53 +0000

    added tests for getting max 3 products - added individual test cases

--- tests/integration/data_access_layer/test_get_max_3_products_for_brand.py
+++ tests/integration/data_access_layer/test_get_max_3_products_for_brand.py
@@ -3,19 +3,54 @@ from src.data_access_layer.read_data_access import load_max_3_products_for_brand
 from tests.unit import InMemorySqliteDataManager, brand_generator, product_generator
 
 
-def test_load_max_3_products_for_brand():
-    data_manager = InMemorySqliteDataManager()
-    brands = [brand_generator(1), brand_generator(2), brand_generator(3), brand_generator(4)]
+def test_load_max_3_products_for_brand_when_many_products_are_available_for_many_brands():
+    brands = [brand_generator(1), brand_generator(2)]
+    products = [product_generator(1, brands[0]),
+                product_generator(2, brands[0]),
+                product_generator(2, brands[1]),
+                product_generator(2, brands[1]),
+                product_generator(2, brands[1])]
+    data_manager = setup_database(brands, products)
+    do_work(products, data_manager)
+
+
+def test_load_max_3_products_for_brand_when_less_than_3_products_are_available():
+    brands = [brand_generator(1)]
+    products = [product_generator(1, brands[0]),
+                product_generator(2, brands[0])]
+    data_manager = setup_database(brands, products)
+    do_work(products, data_manager)
+
+
+def test_load_max_3_products_for_brand_when_more_than_3_products_are_available():
+    brands = [brand_generator(1)]
     products = [product_generator(1, brands[0]),
                 product_generator(2, brands[0]),
                 product_generator(3, brands[0]),
-                product_generator(4, brands[0]),
-                product_generator(1, brands[1]),
-                product_generator(2, brands[1]),
-                product_generator(3, brands[1]),
-                product_generator(1, brands[2])]
+                product_generator(4, brands[0])]
+    data_manager = setup_database(brands, products)
+    do_work(products, data_manager)
+
+
+def test_load_max_3_products_for_brand_when_no_products_are_available():
+    brands = [brand_generator(1)]
+    data_manager = setup_database(brands, [])
+    do_work([], data_manager)
+
+
+def test_load_max_3_products_for_brand_when_brands_are_available():
+    data_manager = setup_database([], [])
+    do_work([], data_manager)
+
+
+def setup_database(brands, products):
+    data_manager = InMemorySqliteDataManager()
     data_manager.create_fake_data(brands)
     data_manager.create_fake_data(products)
+    return data_manager
+
+
+def do_work(products, data_manager):
     items = load_max_3_products_for_brand(data_manager=data_manager, type_=None)
     assert to_list([products[0],
                     products[1],
