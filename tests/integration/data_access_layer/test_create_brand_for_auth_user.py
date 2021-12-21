0007fc5c934bcd6381a5de0d54bfdd3be72e7c53
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-21 08:48:02 +0000

    for test create brand for auth user, added correct assertions against data in db

--- tests/integration/data_access_layer/test_create_brand_for_auth_user.py
+++ tests/integration/data_access_layer/test_create_brand_for_auth_user.py
@@ -4,19 +4,29 @@ from tests import InMemorySqliteDataManager, SpyImageRepo
 
 
 def test_db_write_new_brand_for_auth_user_when_record_is_written_successfully():
-    image_repo = SpyImageRepo({"upload": "test"})
+    test_image = "test.png"
+    image_repo = SpyImageRepo({"upload": test_image})
     data_manager = InMemorySqliteDataManager()
     bytes_ = "test bytes"
-    db_write_new_brand_for_auth_user(auth_user_id="",
-                                     payload={
-                                         "name": "testname",
-                                         "description": "testdescription",
-                                         "website": "testwebsite",
-                                         "email": "testemail",
-                                         "instahandle": "instahandle",
-                                         "image": bytes_
-                                     },
+    test_auth_id = "test auth id"
+    payload = payload = {
+        "name": "testname",
+        "description": "testdescription",
+        "website": "testwebsite",
+        "email": "testemail",
+        "instahandle": "instahandle",
+        "image": bytes_
+    }
+    db_write_new_brand_for_auth_user(auth_user_id=test_auth_id,
+                                     payload=payload,
                                      data_manager=data_manager,
                                      image_repository=image_repo)
-    assert image_repo.received('upload') == 1 and image_repo.with_args('upload') == [
-        f'{data_manager.session.query(Brand).first().id}', bytes_]
+    brand_in_db = data_manager.session.query(Brand).first()
+    assert image_repo.received('upload') == 1 and image_repo.with_args('upload') == [f'{brand_in_db.id}', bytes_]
+    assert brand_in_db.name == payload['name']
+    assert brand_in_db.description == payload['description']
+    assert brand_in_db.website == payload['website']
+    assert brand_in_db.email == payload['email']
+    assert brand_in_db.instahandle == payload['instahandle']
+    assert brand_in_db.image == test_image
+    assert brand_in_db.auth_user_id == test_auth_id
