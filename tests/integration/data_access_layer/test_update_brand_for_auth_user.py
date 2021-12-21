2bd9cc2cb79cd066163f0f053b6aae4e485ac459
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-21 15:37:01 +0000

    add tests for update brand for auth user

---
+++ tests/integration/data_access_layer/test_update_brand_for_auth_user.py
@@ -0,0 +1,41 @@
+from src.data_access_layer.brand import Brand
+from src.data_access_layer.write_data_access import db_write_update_brand_for_auth_user
+from tests import InMemorySqliteDataManager, MockImageRepo, brand_generator
+
+
+def test_db_write_update_brand_for_auth_user_when_successful():
+    [data_manager, image_repository, auth_id, payload] = setup()
+    data_manager.create_fake_data([brand_generator(1, auth_id)])
+    db_write_update_brand_for_auth_user(auth_user_id=auth_id,
+                                        data_manager=data_manager,
+                                        image_repository=image_repository,
+                                        payload=payload)
+    brand_in_db = data_manager.session.query(Brand).first()
+    assert brand_in_db.name == payload['name']
+    assert brand_in_db.description == payload['description']
+    assert brand_in_db.website == payload['website']
+    assert brand_in_db.instahandle == payload['instahandle']
+
+
+def test_db_write_update_brand_for_auth_user_when_brand_doesnt_exist():
+    [data_manager, image_repository, auth_id, payload] = setup()
+    try:
+        db_write_update_brand_for_auth_user(auth_user_id=auth_id,
+                                            data_manager=data_manager,
+                                            image_repository=image_repository,
+                                            payload=payload)
+    except Exception as e:
+        type(e) == IndexError
+
+
+def setup():
+    data_manager = InMemorySqliteDataManager()
+    image_repository = MockImageRepo()
+    auth_id = "authid"
+    payload = {
+        "name": "testname",
+        "description": "testdescription",
+        "website": "testwebsite",
+        "instahandle": "instahandle"
+    }
+    return [data_manager, image_repository, auth_id, payload]
