27ae3bfd4308334630eddffe5dffd92d530cfa3e
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-12-21 18:42:20 +0000

    added test for when image update upload fails for brand

--- tests/integration/data_access_layer/test_update_brand_image_for_auth_user.py
+++ tests/integration/data_access_layer/test_update_brand_image_for_auth_user.py
@@ -1,4 +1,5 @@
 from src.data_access_layer.brand import Brand
+from src.data_access_layer.image_repository import ImageException
 from src.data_access_layer.write_data_access import db_write_patch_brand_image_for_auth_user, \
     NoBrandForAuthenticatedUser
 from tests import InMemorySqliteDataManager, MockImageRepo, brand_generator
@@ -38,3 +39,26 @@ def test_db_write_patch_brand_image_when_brand_does_not_exist():
         pass
     assert image_repo.did_not_receive('upload')
     assert image_repo.did_not_receive('delete')
+
+
+def test_db_write_patch_brand_image_when_upload_image_error_occurs():
+    data_manager = InMemorySqliteDataManager()
+    auth_id = "testauthid"
+    prev_image = "prev.png"
+    brand = brand_generator(1, auth_id, prev_image)
+    data_manager.create_fake_data([brand])
+    bytes_ = "testbytes"
+    image_repo = MockImageRepo({"upload": ImageException()})
+    try:
+        db_write_patch_brand_image_for_auth_user(auth_user_id=auth_id,
+                                                 payload={"image_bytes": bytes_},
+                                                 data_manager=data_manager,
+                                                 image_repository=image_repo)
+        assert False
+    except ImageException:
+        pass
+    assert image_repo.received('upload', 1)
+    assert image_repo.received_with_args('upload', [brand.id, bytes_])
+    assert image_repo.did_not_receive('delete')
+    brand_in_db = data_manager.session.query(Brand).first()
+    assert brand_in_db.image == prev_image
