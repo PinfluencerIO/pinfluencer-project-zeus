4c11e53ad03bf8f2cc8d76a8a7dc463d143054f1
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-23 20:50:28 +0000

    replaced product routes with dummy campaign routes - except for feed

--- src/routes.py
+++ src/routes.py
@@ -1,19 +1,14 @@
 from collections import OrderedDict
 
 from src.data_access_layer.read_data_access import load_max_3_products_for_brand, load_collection, load_by_id, \
-    load_all_products_for_brand_id, load_brand_for_authenticated_user, load_products_for_authenticated_user, \
-    load_product_by_id_for_auth_id
+    load_brand_for_authenticated_user
 from src.data_access_layer.write_data_access import db_write_new_brand_for_auth_user, \
-    db_write_update_brand_for_auth_user, db_write_patch_brand_image_for_auth_user, db_write_new_product_for_auth_user, \
-    db_write_update_product_for_auth_user, db_write_patch_product_image_for_auth_user, delete_product
-from src.processors.delete_product import ProcessAuthenticatedDeleteProduct
+    db_write_update_brand_for_auth_user, db_write_patch_brand_image_for_auth_user
 from src.processors.get_by_id import ProcessGetBy
-from src.processors.get_by_id_for_auth_user import ProcessGetByForAuthenticatedUser
 from src.processors.get_collection import ProcessGetCollection
-from src.processors.get_for_auth_user import ProcessGetForAuthenticatedUser, ProcessGetForAuthenticatedUserAsCollection
-from src.processors.get_products_for_brand import ProcessGetProductsForBrand
-from src.processors.write_for_auth_user import ProcessWriteForAuthenticatedUser, \
-    ProcessWriteWithValidationForAuthenticatedUser, ProcessWriteForAuthenticatedUserWithProductId
+from src.processors.get_for_auth_user import ProcessGetForAuthenticatedUser
+from src.processors.ok_response import ProcessOkResponse
+from src.processors.write_for_auth_user import ProcessWriteForAuthenticatedUser
 
 
 class Routes:
@@ -30,12 +25,11 @@ class Routes:
 
             'GET /brands/{brand_id}': ProcessGetBy(load_by_id, 'brand', self.__container.data_manager),
 
-            'GET /brands/{brand_id}/products': ProcessGetProductsForBrand(load_all_products_for_brand_id,
-                                                                          self.__container.data_manager),
+            'GET /brands/{brand_id}/campaigns': ProcessOkResponse(),
 
-            'GET /products': ProcessGetCollection('product', load_collection, self.__container.data_manager),
+            'GET /campaigns': ProcessOkResponse(),
 
-            'GET /products/{product_id}': ProcessGetBy(load_by_id, 'product', self.__container.data_manager),
+            'GET /campaigns/{campaign_id}': ProcessOkResponse(),
 
             # authenticated brand endpoints
             'GET /brands/me': ProcessGetForAuthenticatedUser(load_brand_for_authenticated_user,
@@ -54,29 +48,16 @@ class Routes:
                                                                        self.__container.data_manager,
                                                                        self.__container.image_repository),
 
-            # authenticated product endpoints
-            'GET /products/me': ProcessGetForAuthenticatedUserAsCollection(load_products_for_authenticated_user,
-                                                                           self.__container.data_manager),
+            # authenticated campaign endpoints
+            'GET /campaigns/me': ProcessOkResponse(),
 
-            'GET /products/me/{product_id}': ProcessGetByForAuthenticatedUser(load_product_by_id_for_auth_id, 'product',
-                                                                              self.__container.data_manager),
+            'GET /campaigns/me/{campaign_id}': ProcessOkResponse(),
 
-            'POST /products/me': ProcessWriteWithValidationForAuthenticatedUser('product', 'post',
-                                                                                db_write_new_product_for_auth_user,
-                                                                                self.__container.data_manager,
-                                                                                self.__container.image_repository),
+            'POST /campaigns/me': ProcessOkResponse(),
 
-            'PUT /products/me/{product_id}':
-                ProcessWriteForAuthenticatedUserWithProductId('product', 'put', db_write_update_product_for_auth_user,
-                                                              self.__container.data_manager,
-                                                              self.__container.image_repository),
+            'PUT /campaigns/me/{campaign_id}': ProcessOkResponse(),
 
-            'PATCH /products/me/{product_id}/image':
-                ProcessWriteForAuthenticatedUserWithProductId('product', 'patch',
-                                                              db_write_patch_product_image_for_auth_user,
-                                                              self.__container.data_manager,
-                                                              self.__container.image_repository),
+            'PATCH /campaigns/me/{campaign_id}/image': ProcessOkResponse(),
 
-            'DELETE /products/me/{product_id}': ProcessAuthenticatedDeleteProduct(delete_product,
-                                                                                  self.__container.data_manager),
+            'DELETE /campaigns/me/{campaign_id}': ProcessOkResponse(),
         })
