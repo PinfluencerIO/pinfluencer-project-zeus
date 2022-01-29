dc65501dfa59c337b568fb2e06e6f3b480656d74
Dom Farr <dominicfarr@gmail.com>
2022-01-29 11:48:54 -0400

    models file with model classes example

---
+++ src/data_access_layer/models.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+class Brand:
+    pass
+
+
+class Influencer:
+    pass
+
+
+class Campaign:
+    pass
+
+
+class Collaboration:
+    pass
+
+
+@dataclass(frozen=True)
+class CollaborationRequest:
+    pass
