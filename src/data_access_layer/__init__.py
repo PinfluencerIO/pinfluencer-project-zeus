7d7d3eb0a047e7a2574e807e17172f5485802143
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-31 02:35:33 +0000

    added model DTOs

--- src/data_access_layer/__init__.py
+++ src/data_access_layer/__init__.py
@@ -1,7 +1,6 @@
 import uuid
 from dataclasses import dataclass
 from datetime import datetime
-from enum import Enum
 
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -34,32 +33,6 @@ def to_list(data):
     for data_item in data:
         data_dict.append(data_item.as_dict())
     return data_dict
-
-
-class ValueEnum(Enum):
-    Sustainable = "Sustainable"
-    Organic = "Organic"
-    Recycled = "Recycled"
-    Vegan = "Vegan"
-    Value5 = "Value5"
-    Value6 = "Value6"
-    Value7 = "Value7"
-    Value8 = "Value8"
-    Value9 = "Value9"
-    Value10 = "Value10"
-
-
-class CategoryEnum(Enum):
-    Food = "Food"
-    Fashion = "Fashion"
-    Fitness = "Fitness"
-    Pet = "Pet"
-    Category5 = "Category5"
-    Category6 = "Category6"
-    Category7 = "Category7"
-    Category8 = "Category8"
-    Category9 = "Category9"
-    Category10 = "Category10"
 
 
 class BaseUser(BaseEntity):
---
+++ src/data_access_layer/models.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+
+
+class GenderEnum(Enum):
+    Male = "Male"
+    Female = "Female"
+
+
+@dataclass
+class Metric:
+    Value: float
+
+
+@dataclass
+class AudienceAge(Metric):
+    Min: int
+    Max: int
+
+
+@dataclass
+class AudienceGender(Metric):
+    Gender: GenderEnum
+
+
+class ValueEnum(Enum):
+    Sustainable = "Sustainable"
+    Organic = "Organic"
+    Recycled = "Recycled"
+    Vegan = "Vegan"
+    Value5 = "Value5"
+    Value6 = "Value6"
+    Value7 = "Value7"
+    Value8 = "Value8"
+    Value9 = "Value9"
+    Value10 = "Value10"
+
+
+class CategoryEnum(Enum):
+    Food = "Food"
+    Fashion = "Fashion"
+    Fitness = "Fitness"
+    Pet = "Pet"
+    Category5 = "Category5"
+    Category6 = "Category6"
+    Category7 = "Category7"
+    Category8 = "Category8"
+    Category9 = "Category9"
+    Category10 = "Category10"
+
+
+@dataclass
+class User:
+    id: str
+    created: datetime
+
+
+@dataclass
+class Brand(User):
+    name: str
+    description: str
+    website: str
+    logo: str
+    header_image: str
+    instahandle: str
+    values: list[ValueEnum]
+    categories: list[CategoryEnum]
+
+
+@dataclass
+class Influencer(User):
+    instahandle: str
+    name: str
+    website: str
+    bio: str
+    image: str
+    audience_age_split: list[AudienceAge]
+    audience_gender_split: list[AudienceGender]
+    values: list[ValueEnum]
+    categories: list[CategoryEnum]
