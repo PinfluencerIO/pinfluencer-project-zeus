bd60daff5c751586880872e76e281f4518684a25
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-01-31 12:54:33 +0000

    add base serialization methods for entities

--- src/data_access_layer/entities.py
+++ src/data_access_layer/entities.py
@@ -13,6 +13,10 @@ class BaseEntity:
     id = Column(String(length=36), primary_key=True, nullable=False)
     created = Column(DateTime, nullable=False)
 
+    def from_dto(self, dto):
+        self.id = dto.id
+        self.created = dto.created
+
 
 class BaseUserEntity(BaseEntity):
     first_name = Column(type_=String(length=120), nullable=False)
@@ -20,28 +24,32 @@ class BaseUserEntity(BaseEntity):
     email = Column(type_=String(length=120), nullable=False)
     auth_user_id = Column(type_=String(length=64), nullable=False, unique=True)
 
+    def from_dto(self, dto):
+        super().from_dto(dto=dto)
+        self.first_name = dto.first_name
+        self.last_name = dto.last_name
+        self.email = dto.email
+        self.auth_user_id = dto.auth_user_id
+
 
 class BrandEntity(Base, BaseUserEntity):
     __tablename__ = BRAND_TBL_NAME
 
     @staticmethod
-    def from_dto(dto=None):
-        brand = BrandEntity()
-        brand.id = dto.id
-        brand.created = dto.created
-        brand.first_name = dto.first_name
-        brand.last_name = dto.last_name
-        brand.email = dto.email
-        brand.auth_user_id = dto.auth_user_id
-        brand.name = dto.name
-        brand.description = dto.description
-        brand.header_image = dto.header_image
-        brand.values = json.dumps(list(map(lambda x: x.name, dto.values)))
-        brand.categories = json.dumps(list(map(lambda x: x.name, dto.categories)))
-        brand.instahandle = dto.instahandle
-        brand.website = dto.website
-        brand.logo = dto.logo
-        return brand
+    def brand_from_dto(dto=None):
+        return BrandEntity().from_dto(dto)
+
+    def from_dto(self, dto):
+        super().from_dto(dto=dto)
+        self.name = dto.name
+        self.description = dto.description
+        self.header_image = dto.header_image
+        self.values = json.dumps(list(map(lambda x: x.name, dto.values)))
+        self.categories = json.dumps(list(map(lambda x: x.name, dto.categories)))
+        self.instahandle = dto.instahandle
+        self.website = dto.website
+        self.logo = dto.logo
+        return self
 
     name = Column(type_=String(length=120), nullable=False)
     description = Column(type_=String(length=500), nullable=False)
