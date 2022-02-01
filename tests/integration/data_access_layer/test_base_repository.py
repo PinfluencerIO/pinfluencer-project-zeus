2244a5f3161235bc5cf57663cc0313812efbc439
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-02-01 02:06:45 +0000

    add test case class for base repository tests

--- tests/integration/data_access_layer/test_base_repository.py
+++ tests/integration/data_access_layer/test_base_repository.py
@@ -1,23 +1,21 @@
+from unittest import TestCase
+
 from src.data_access_layer.repositories import BrandRepository
 from tests import InMemorySqliteDataManager, brand_generator, brand_dto_generator
 
 
-def test_load_brand_by_id_item_when_brand_can_be_found():
-    [data_manager, unexpected_brand] = setup_database()
-    repository = BrandRepository(data_manager=data_manager, image_repository=None)
-    actual_brand = repository.load_by_id(id_=unexpected_brand.id)
-    assert unexpected_brand.__dict__ == actual_brand.__dict__
+class TestBaseRepository(TestCase):
 
+    def setUp(self):
+        self.__data_manager = InMemorySqliteDataManager()
+        self.__repository = BrandRepository(data_manager=self.__data_manager, image_repository=None)
 
-def test_load_brand_item_by_id_when_brand_cannot_be_found():
-    [data_manager, _] = setup_database()
-    repository = BrandRepository(data_manager=data_manager, image_repository=None)
-    actual_brand = repository.load_by_id(id_="1234")
-    assert actual_brand is None
+    def test_load_brand_by_id_item_when_brand_can_be_found(self):
+        unexpected_brand = brand_dto_generator(1)
+        self.__data_manager.create_fake_data([brand_generator(unexpected_brand)])
+        actual_brand = self.__repository.load_by_id(id_=unexpected_brand.id)
+        assert unexpected_brand.__dict__ == actual_brand.__dict__
 
-
-def setup_database():
-    data_manager = InMemorySqliteDataManager()
-    brand = brand_dto_generator(1)
-    data_manager.create_fake_data([brand_generator(brand)])
-    return [data_manager, brand]
+    def test_load_brand_item_by_id_when_brand_cannot_be_found(self):
+        actual_brand = self.__repository.load_by_id(id_="1234")
+        assert actual_brand is None
