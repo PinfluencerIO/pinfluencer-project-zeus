e28de91f590acb43b5005735bbeb2b5d58903a41
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-02-01 03:24:22 +0000

    add test case for get auth brand when not found brand controller - test fails

--- tests/unit/test_brand_controller.py
+++ tests/unit/test_brand_controller.py
@@ -59,3 +59,11 @@ class TestBrandController(TestCase):
         self.__brand_repository.load_for_auth_user.assert_called_once_with(auth_user_id=auth_id)
         assert response.body == expected_brand.__dict__
         assert response.status_code == 200
+
+    def test_handle_get_brand_when_brant_not_found(self):
+        self.__brand_repository.load_for_auth_user = MagicMock(return_value=None)
+        auth_id = "1234brand1"
+        response = self.__sut.handle_get_brand({"requestContext": {"authorizer": {"jwt": {"claims":{"cognito:username": auth_id}}}}})
+        self.__brand_repository.load_for_auth_user.assert_called_once_with(auth_user_id=auth_id)
+        assert response.body == {}
+        assert response.status_code == 404
