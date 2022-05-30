db91fe1fdb39f6cbe23852b5901a4439daff38d7
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-05-30 02:02:01 +0100

    add passing implementation for serializer

--- src/crosscutting/__init__.py
+++ src/crosscutting/__init__.py
@@ -1,3 +1,6 @@
+import json
+
+
 def print_exception(e):
     print(''.join(['Exception ', str(type(e))]))
     print(''.join(['Exception ', str(e)]))
@@ -6,7 +9,11 @@ def print_exception(e):
 class JsonSnakeToCamelSerializer:
 
     def serialize(self, data: dict) -> str:
-        return ""
+        return json.dumps({self.__snake_case_key_to_camel_case(k): v for k, v in data.items()})
+
+    def __snake_case_key_to_camel_case(self, key: str) -> str:
+        components = key.split('_')
+        return components[0] + ''.join(x.title() for x in components[1:])
 
 
 class JsonCamelToSnakeDeserializer:
