5e629ca6972b3d4cec6fc91253f2d9a301ba0ec5
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-05-31 01:50:24 +0100

    add serialization/deserialization test for caps

--- tests/test_crosscutting.py
+++ tests/test_crosscutting.py
@@ -2,6 +2,23 @@ from unittest import TestCase
 
 from src.crosscutting import JsonSnakeToCamelSerializer, JsonCamelToSnakeCaseDeserializer
 
+TEST_DICT_JSON = "{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2}"
+TEST_DICT_JSON_WITH_CAPS_KEY = "{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2, \"capitalLETTERSValue\": 1}"
+
+TEST_DICT = {
+    "name": "adam raymond",
+    "snake_in_value": "snake_in_value",
+    "value_2_to_3_values": 2
+}
+
+
+TEST_DICT_WITH_CAPS_KEY = {
+    "name": "adam raymond",
+    "snake_in_value": "snake_in_value",
+    "value_2_to_3_values": 2,
+    "capital_letters_value": 1
+}
+
 
 class TestJsonSnakeToCamelSerializer(TestCase):
 
@@ -9,14 +26,9 @@ class TestJsonSnakeToCamelSerializer(TestCase):
         self.__json_snake_to_camel_serializer = JsonSnakeToCamelSerializer()
 
     def test_serialize(self):
-        input_data = {
-            "name": "adam raymond",
-            "snake_in_value": "snake_in_value",
-            "value_2_to_3_values": 2
-        }
 
-        expected =\
-            "{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2}"
+        input_data = TEST_DICT
+        expected = TEST_DICT_JSON
 
         actual = self.__json_snake_to_camel_serializer.serialize(input_data)
 
@@ -29,14 +41,9 @@ class TestJsonCamelToSnakeCaseDeserializer(TestCase):
         self.__json_camel_to_snake_case_deserializer = JsonCamelToSnakeCaseDeserializer()
 
     def test_deserialize(self):
-        expected = {
-            "name": "adam raymond",
-            "snake_in_value": "snake_in_value",
-            "value_2_to_3_values": 2
-        }
 
-        input_data =\
-            "{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2}"
+        expected = TEST_DICT_WITH_CAPS_KEY
+        input_data = TEST_DICT_JSON_WITH_CAPS_KEY
 
         actual = self.__json_camel_to_snake_case_deserializer.deserialize(input_data)
 
