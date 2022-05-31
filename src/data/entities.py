9316a09dbe5c38e68508e0570cb5c297db7651bd
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-05-31 16:51:51 +0100

    change influencer fields to adhere to root/schema.txt - with refactor

--- src/data/entities.py
+++ src/data/entities.py
@@ -35,20 +35,19 @@ class BrandEntity(Base, BaseUserEntity):
 class InfluencerEntity(Base, BaseUserEntity):
     __tablename__ = 'influencer'
 
-    name = Column(type_=String(length=120), nullable=False)
     website = Column(type_=String(length=120), nullable=False)
     bio = Column(type_=String(length=500), nullable=False)
     image = Column(type_=String(length=360), nullable=True)
-    audience_age_13_17_split = Column(type_=Float, nullable=True)
-    audience_age_18_24_split = Column(type_=Float, nullable=True)
-    audience_age_25_34_split = Column(type_=Float, nullable=True)
-    audience_age_35_44_split = Column(type_=Float, nullable=True)
-    audience_age_45_54_split = Column(type_=Float, nullable=True)
-    audience_age_55_64_split = Column(type_=Float, nullable=True)
+    audience_age_13_to_17_split = Column(type_=Float, nullable=True)
+    audience_age_18_to_24_split = Column(type_=Float, nullable=True)
+    audience_age_25_to_34_split = Column(type_=Float, nullable=True)
+    audience_age_35_to_44_split = Column(type_=Float, nullable=True)
+    audience_age_45_to_54_split = Column(type_=Float, nullable=True)
+    audience_age_55_to_64_split = Column(type_=Float, nullable=True)
     audience_age_65_plus_split = Column(type_=Float, nullable=True)
     audience_male_split = Column(type_=Float, nullable=True)
     audience_female_split = Column(type_=Float, nullable=True)
-    instahandle = Column(type_=String(length=30), nullable=True)
+    insta_handle = Column(type_=String(length=30), nullable=True)
     values = Column(type_=PickleType, nullable=False)
     categories = Column(type_=PickleType, nullable=False)
 
