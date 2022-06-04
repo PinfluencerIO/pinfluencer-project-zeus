2ef22338a4abc918c4fae7c0492fe2c639b7b4b7
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-06-04 13:52:10 +0100

    add test for nested dict serialization and list serialization

--- tests/test_crosscutting.py
+++ tests/test_crosscutting.py
@@ -3,12 +3,40 @@ from unittest import TestCase
 from src.crosscutting import JsonSnakeToCamelSerializer, JsonCamelToSnakeCaseDeserializer
 
 TEST_DICT_JSON = "{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2}"
+TEST_LIST_SERIALIZATION_JSON = "[{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2}, {\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2}, {\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2}]"
+TEST_DICT_NESTED_JSON = "{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2, \"nestedObject\": {\"name\": \"dennis reynolds\", \"snakeValue\": 3}, \"arrayValue\": [\"apples\", \"pears\", \"oranges\"]}"
 TEST_DICT_JSON_WITH_CAPS_KEY = "{\"name\": \"adam raymond\", \"snakeInValue\": \"snake_in_value\", \"value2To3Values\": 2, \"capitalLETTERSValue\": 1}"
 
 TEST_DICT = {
     "name": "adam raymond",
     "snake_in_value": "snake_in_value",
     "value_2_to_3_values": 2
+}
+
+
+TEST_LIST_SERIALIZATION = [{
+    "name": "adam raymond",
+    "snake_in_value": "snake_in_value",
+    "value_2_to_3_values": 2
+},{
+    "name": "adam raymond",
+    "snake_in_value": "snake_in_value",
+    "value_2_to_3_values": 2
+},{
+    "name": "adam raymond",
+    "snake_in_value": "snake_in_value",
+    "value_2_to_3_values": 2
+}]
+
+TEST_NESTED_DICT = {
+    "name": "adam raymond",
+    "snake_in_value": "snake_in_value",
+    "value_2_to_3_values": 2,
+    "nested_object": {
+        "name": "dennis reynolds",
+        "snake_value": 3
+    },
+    "array_value": ["apples", "pears", "oranges"]
 }
 
 
@@ -29,6 +57,24 @@ class TestJsonSnakeToCamelSerializer(TestCase):
 
         input_data = TEST_DICT
         expected = TEST_DICT_JSON
+
+        actual = self.__json_snake_to_camel_serializer.serialize(input_data)
+
+        assert expected == actual
+
+    def test_serialize_nested(self):
+
+        input_data = TEST_NESTED_DICT
+        expected = TEST_DICT_NESTED_JSON
+
+        actual = self.__json_snake_to_camel_serializer.serialize(input_data)
+
+        assert expected == actual
+
+    def test_serialize_collection(self):
+
+        input_data = TEST_LIST_SERIALIZATION
+        expected = TEST_LIST_SERIALIZATION_JSON
 
         actual = self.__json_snake_to_camel_serializer.serialize(input_data)
 
