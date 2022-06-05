a7079c631644815b73bae168ed9c208081686439
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-06-05 16:52:54 +0100

    fix template file path error in CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import os
 from typing import Union
 from unittest import TestCase
 from unittest.mock import Mock, MagicMock
@@ -147,7 +148,7 @@ class TestRoutes(TestCase):
             route_key="POST /campaigns/me")
 
     def test_template_matches_routes(self):
-        with open("./../template.yaml") as file:
+        with open(f"{os.path.abspath('..')}/template.yaml") as file:
             yaml_str = file.read()
             data = load_yaml(yaml_str)
             paths = sorted([f"{event[1]['Properties']['Method'].upper()} {event[1]['Properties']['Path']}" for event in data["Resources"]["PinfluencerFunction"]["Properties"]["Events"].items()])
