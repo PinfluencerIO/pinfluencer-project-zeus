150de69b53ef271f4a267b027098232a0bf87e3e
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-06-26 14:24:51 +0100

    modify tag claims to single model hook to get auth user id from body rather than event to comply with all endpoints

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -18,7 +18,6 @@ class TestUserAfterHooks(TestCase):
         # arrange
         brand = brand_dto_generator(num=1, repo=RepoEnum.STD_REPO)
         response = PinfluencerResponse(body=brand.__dict__)
-        auth_user_id = "12341"
         auth_user = User(first_name="cognito_first_name",
                          last_name="cognito_last_name",
                          email="cognito_email")
@@ -26,14 +25,13 @@ class TestUserAfterHooks(TestCase):
 
         # act
         self.__sut.tag_auth_user_claims_to_response(context=PinfluencerContext(response=response,
-                                                                               event={},
-                                                                               auth_user_id=auth_user_id))
+                                                                               event={}))
 
         # assert
         assert response.body["first_name"] == auth_user.first_name
         assert response.body["last_name"] == auth_user.last_name
         assert response.body["email"] == auth_user.email
-        self.__auth_user_repository.get_by_id.assert_called_once_with(_id=auth_user_id)
+        self.__auth_user_repository.get_by_id.assert_called_once_with(_id=brand.auth_user_id)
 
     def test_tag_auth_user_claims_to_response_collection(self):
 
