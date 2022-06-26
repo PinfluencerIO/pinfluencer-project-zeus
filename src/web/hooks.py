80bab659f9915acaba8757b937472f676d496a11
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-06-26 13:51:13 +0100

    add implementation for tagging cognito claims to single model response

--- src/web/hooks.py
+++ src/web/hooks.py
@@ -8,4 +8,7 @@ class UserAfterHooks:
         self.__auth_user_repository = auth_user_repository
 
     def tag_auth_user_claims_to_response(self, context: PinfluencerContext):
-        ...
+        auth_user = self.__auth_user_repository.get_by_id(_id=context.auth_user_id)
+        context.response.body["first_name"] = auth_user.first_name
+        context.response.body["last_name"] = auth_user.last_name
+        context.response.body["email"] = auth_user.email
