22c44b6594655c189e653644d9c003ebd5a2c670
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-06-28 00:57:05 +0100

    add implementation for brand middleware structure

--- src/web/routing.py
+++ src/web/routing.py
@@ -27,11 +27,13 @@ class Dispatcher:
 
         users = OrderedDict(
             {
-                'GET /brands': Route(action=self.__brand_ctr.get_all),
+                'GET /brands': Route(action=self.__brand_ctr.get_all,
+                                     after_hooks=[self.__hooks_facade.get_user_after_hooks().tag_auth_user_claims_to_response_collection]),
 
                 'GET /influencers': Route(action=self.__influencer_ctr.get_all),
 
-                'GET /brands/{brand_id}': Route(before_hooks=[self.__hooks_facade.get_brand_before_hooks().validate_uuid], action=self.__brand_ctr.get_by_id),
+                'GET /brands/{brand_id}': Route(before_hooks=[self.__hooks_facade.get_brand_before_hooks().validate_uuid],
+                                                action=self.__brand_ctr.get_by_id),
 
                 'GET /influencers/{influencer_id}': Route(action=self.__influencer_ctr.get_by_id),
 
